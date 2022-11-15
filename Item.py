029f5823ae168c5f8358c86daf9019bdecace9fa
Josep Bernad <jbernadespinosa@gmail.com>
2022-11-15 22:33:14 +0100

    Fix and improve Brute Force Test. Fix item increase and decrease methods.

--- Item.py
+++ Item.py
@@ -1,17 +1,23 @@
 class Item:
+    MAX_QUALITY = 50
+    MIN_QUALITY = 0
+
     def __init__(self, name, sell_in, quality):
         self.name = name
         self.sell_in = sell_in
         self.quality = quality
 
     def increaseQuality(self):
-        self.increaseQualityBy(1)
+        if self.quality < self.MAX_QUALITY:
+            self.quality += 1
 
     def decreaseQuality(self):
-        self.quality = max(0, self.quality - 1)
+        if self.quality > self.MIN_QUALITY:
+            self.quality -= 1
 
     def increaseQualityBy(self, n):
-        self.quality = min(50, self.quality + n)
+        for i in range(0, n):
+            self.increaseQuality()
 
     def decreaseSellIn(self):
         self.sell_in = self.sell_in - 1
--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -1,13 +1,13 @@
 import unittest
+import logging
 
 from GildedRoseJosep import GildedRoseJosep
 from gilded_rose import GildedRose
 from Item import Item
 
-
 class GildedRoseTest(unittest.TestCase):
 
-    names = [
+    NAMES = [
         "Aged Brie",
         "Backstage passes to a TAFKAL80ETC concert",
         "Sulfuras, Hand of Ragnaros",
@@ -15,28 +15,32 @@ class GildedRoseTest(unittest.TestCase):
     ]
 
     def testBruteForce(self):
-        iterations = 5
+        days = 2
 
         itemsOriginal = []
+        itemsProcessed = []
+        itemsProcessedOriginal = []
 
         for i in range(-100, 100):
             for j in range(-100, 100):
-                for name in self.names:
+                for name in self.NAMES:
                     itemsOriginal.append(Item(name=name, sell_in=i, quality=j))
+                    itemsProcessed.append(Item(name=name, sell_in=i, quality=j))
+                    itemsProcessedOriginal.append(Item(name=name, sell_in=i, quality=j))
 
-        itemsProcessed = itemsOriginal.copy()
-
-        for i in range(1, iterations):
+        for i in range(1, days):
             GildedRose(itemsOriginal).update_quality()
             GildedRoseJosep(itemsProcessed).updateQuality()
 
             for j in range(len(itemsOriginal) - 1):
-                if itemsOriginal[i].quality != itemsProcessed[i].quality or \
-                        itemsOriginal[i].sell_in != itemsProcessed[i].sell_in:
-                    print(itemsOriginal[i])
-                    print(itemsProcessed[i])
-                    self.assertEqual(itemsOriginal[i].quality, itemsProcessed[i].quality)
-                    self.assertEqual(itemsOriginal[i].sell_in, itemsProcessed[i].sell_in)
+                if itemsOriginal[j].quality != itemsProcessed[j].quality or \
+                        itemsOriginal[j].sell_in != itemsProcessed[j].sell_in:
+                    print("------ " + i.__str__() + " ------")
+                    print("Item original:" + itemsOriginal[j].__repr__())
+                    print("Item processed original:" + itemsProcessedOriginal[j].__repr__())
+                    print("Item processed:" + itemsProcessed[j].__repr__())
+                    self.assertEqual(itemsOriginal[j].quality, itemsProcessed[j].quality)
+                    self.assertEqual(itemsOriginal[j].sell_in, itemsProcessed[j].sell_in)
 
 
 if __name__ == '__main__':
